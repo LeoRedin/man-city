5c6d9161c8d76ed390e042d1f380e93b96e366a3
Leonardo Redin <leonardo.redin@ezcommerce.local>
2018-12-13 17:48:37 -0200

    Ajuste de Rotas

--- src/Routes.js
+++ src/Routes.js
@@ -10,7 +10,6 @@ import Login from "./Components/Login";
 
 import Dashboard from "./Components/Admin/Dashboard";
 import AdminMatches from "./Components/Admin/Matches";
-import AddOrEditMatch from "./Components/Admin/Matches/AddOrEditMatch";
 
 const Routes = props => {
     return (
@@ -18,38 +17,29 @@ const Routes = props => {
             <Switch>
                 <PrivateRoute
                     {...props}
-                    path="/admin_partidas/editar_partida/:id"
-                    exact
-                    component={AddOrEditMatch}
-                />
-
-                <PrivateRoute
-                    {...props}
                     path="/admin_partidas"
                     exact
                     component={AdminMatches}
                 />
-
                 <PrivateRoute
                     {...props}
                     path="/dashboard"
                     exact
                     component={Dashboard}
                 />
-
+                <PublicRoute
+                    {...props}
+                    restricted={true}
+                    path="/login"
+                    exact
+                    component={Login}
+                />
                 <PublicRoute
                     {...props}
                     restricted={false}
                     path="/"
                     exact
                     component={Home}
-                />
-                <PublicRoute
-                    {...props}
-                    restricted={true}
-                    path="/"
-                    exact
-                    component={Login}
                 />
             </Switch>
         </Layout>
